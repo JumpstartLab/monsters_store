28fcc115213edc6aa35a74905497f0642c9630bc
Jeff Casimir <jeff@casimircreative.com>
2013-08-09 10:45:58 -0700

    Strip validations to external app

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,12 +1,6 @@
 class Rating < ActiveRecord::Base
   attr_accessible :body, :stars, :title, :user_id, :product_id
 
-  validates :user_id, presence: true
-  validates :product_id, presence: true
-  validates :title, presence: true
-  validates :body, presence: true
-  validates :stars, presence: true, inclusion: 0..5
-
   def user
     @user ||= User.find(user_id)
   end
